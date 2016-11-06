0c7835b509d9834b5967fd313f1a2a84b7a7f7e0
Catherine Holloway <milankie@gmail.com>
2016-11-06 14:09:51 -0800

    experimenting with delays

--- raspi/main.py
+++ raspi/main.py
@@ -1,10 +1,14 @@
+from time import sleep
+
 from serial import Serial
-conn = Serial('/dev/ttyAMA0', baudrate=460800)
+conn = Serial('/dev/ttyAMA0', baudrate=460800, timeout=2)
 while True:
     conn.write(b'x')
     x_val = conn.read(2)
+    sleep(1)
     conn.write(b'y')
     y_val = conn.read(2)
+    sleep(1)
     conn.write(b'z')
     z_val = conn.read(2)
     print(int(x_val), int(y_val), int(z_val))
