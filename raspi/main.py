346b81110e163afa89f4b31b08b97ceeaee513b6
Catherine Holloway <milankie@gmail.com>
2016-11-06 14:16:19 -0800

    re-assembling ints

--- raspi/main.py
+++ raspi/main.py
@@ -4,11 +4,14 @@ from serial import Serial
 conn = Serial('/dev/ttyAMA0', baudrate=460800, timeout=2)
 while True:
     conn.write(b'x')
-    x_val = conn.read(2)
+    x_val = ord(conn.read(1))
+    x_val += ord(conn.read(1)) << 8
     sleep(1)
     conn.write(b'y')
-    y_val = conn.read(2)
+    y_val = ord(conn.read(1))
+    y_val += ord(conn.read(1)) << 8
     sleep(1)
     conn.write(b'z')
-    z_val = conn.read(2)
-    print(int(x_val), int(y_val), int(z_val))
+    z_val = ord(conn.read(1))
+    z_val += ord(conn.read(1)) << 8
+    print(x_val, y_val, z_val)
