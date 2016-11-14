d532d31ba03e409acbb291403545298878e3e342
Catherine Holloway <milankie@gmail.com>
2016-11-14 14:12:28 -0800

    changed pins

--- raspi/parallel/test_pins.py
+++ raspi/parallel/test_pins.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
-from time import time, sleep
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-data_pins = [4, 17, 21, 22, 14, 15, 18, 23, 24, 10]
+data_pins = [4, 17, 21, 22, 25, 15, 18, 23, 24, 10]
 
 for i in range(len(data_pins)):
     GPIO.setup(data_pins[i], GPIO.OUT)
@@ -12,9 +12,9 @@ i = 0
 while True:
     print(i, data_pins[i])
     GPIO.output(data_pins[i], 1)
-    sleep(1)
+    sleep(0.5)
     GPIO.output(data_pins[i], 0)
-    sleep(1)
+    sleep(0.5)
     i += 1
     if i > 9:
         i = 0
