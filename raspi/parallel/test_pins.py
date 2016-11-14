7921e197a334e0bd1a5e74c330718d13170a888d
Catherine Holloway <milankie@gmail.com>
2016-11-14 14:16:30 -0800

    changed 14 to 15

--- raspi/parallel/test_pins.py
+++ raspi/parallel/test_pins.py
@@ -3,7 +3,7 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-data_pins = [4, 17, 21, 22, 14, 25, 18, 23, 24, 10]
+data_pins = [4, 17, 21, 22, 15, 25, 18, 23, 24, 10]
 
 for i in range(len(data_pins)):
     GPIO.setup(data_pins[i], GPIO.OUT)
