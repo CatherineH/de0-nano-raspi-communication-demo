a1a2bbb551344c8894f7107f8b6980e044c4bec5
Catherine Holloway <milankie@gmail.com>
2016-11-14 14:08:47 -0800

    adding two other pins

--- raspi/parallel/test_pins.py
+++ raspi/parallel/test_pins.py
@@ -3,13 +3,14 @@ from time import time, sleep
 
 GPIO.setmode(GPIO.BCM)
 
-data_pins = [1, 0, 4, 17, 21, 22, 14, 15, 18, 23]
+data_pins = [4, 17, 21, 22, 14, 15, 18, 23, 24, 10]
 
 for i in range(len(data_pins)):
     GPIO.setup(data_pins[i], GPIO.OUT)
 
 i = 0
 while True:
+    print(i, data_pins[i])
     GPIO.output(data_pins[i], 1)
     sleep(1)
     GPIO.output(data_pins[i], 0)
