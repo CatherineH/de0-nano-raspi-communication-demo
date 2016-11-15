3166959bacca73379b39c99dd07e258318819e15
Catherine Holloway <milankie@gmail.com>
2016-11-15 21:14:54 +0000

    changed main.py

--- raspi/parallel/main.py
+++ raspi/parallel/main.py
@@ -1,7 +1,7 @@
 # high chip select means writing, low means reading
 # clock is negative edged
 import RPi.GPIO as GPIO
-from time import time
+from time import time, sleep
 
 GPIO.setmode(GPIO.BCM)
 
@@ -13,17 +13,19 @@ GPIO.setup(clock_pin, GPIO.OUT)
 GPIO.setup(chip_select, GPIO.OUT)
 
 
-def read_dimension(dimension):
+def send_byte(byte_out):
     GPIO.output(clock_pin, 0)
     # set the chip select to write
     GPIO.output(chip_select, 1)
-    # first, set the dimension (x, y, z)
-    values = [(ord(dimension) >> i) % 2 for i in range(0, 8)]
+    # send the byte 
+    values = [(ord(byte_out) >> i) % 2 for i in range(0, 8)]
     GPIO.setup(data_pins, GPIO.OUT)
     GPIO.output(data_pins, values)
     # flash the clock pin
     GPIO.output(clock_pin, 1)
-    GPIO.output(clock_pin, 0)
+    GPIO.output(clock_pin, 0)   
+
+def get_byte():
     # read the data pins
     GPIO.output(chip_select, 0)
     GPIO.output(clock_pin, 1)
@@ -32,18 +34,40 @@ def read_dimension(dimension):
     value = 0
     for i in range(0, 8):
         value += GPIO.input(data_pins[i]) << i
-    # do it twice
-    GPIO.output(clock_pin, 1)
-    GPIO.output(clock_pin, 0)
-    GPIO.setup(data_pins, GPIO.IN)
-    for i in range(0, 8):
-        value += GPIO.input(data_pins[i]) << (i+8)
     return value
 
+def read_dimension(dimension):
+    # first, set the dimension (x, y, z)
+    send_byte(dimension)
+    first_byte = get_byte()
+    send_byte(dimension.upper())
+    second_byte = get_byte()
+    #print(first_byte, second_byte)
+    return first_byte + second_byte << 8
 
-while True:
-    start = time()
-    x_val = read_dimension(b'x')
-    y_val = read_dimension(b'y')
-    z_val = read_dimension(b'z')
-    print(x_val, y_val, z_val, time()-start)
+def echo_box():
+    byte_out = 0
+    while True:
+        start = time()
+        send_byte(chr(byte_out))
+        byte_out += 1
+        if byte_out > 255:
+            byte_out = 0
+        byte_in = get_byte()
+        diff = byte_out-byte_in
+        end = time()
+        print("diff: ", diff, start-end)
+
+def acc():
+    while True:
+        start = time()
+        x_val = read_dimension(b'x')
+        sleep(0.1)
+        y_val = read_dimension(b'y')
+        sleep(0.1)
+        z_val = read_dimension(b'z')
+        sleep(0.1)
+        print(x_val, y_val, z_val, time()-start)
+
+if __name__ == "__main__":
+    acc()
